b2b976d90f12281d44c34bee10cab589a3690dff
ratimir-matana <ratimir.matana@gmail.com>
2021-08-15 17:04:37 +0200

    [Odoo14 - Minimal module example] Minimalistic module example that can be installed Minimalistički primjer modula koji se može instalirati [minimal_module]

---
+++ addons_lte/minimal_module/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': "Minimal Module",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Ratimir Matana",
+    'category': 'Category',
+    'description': """
+    This is minimal Odoo module example which can be installed
+    """,
+    # data files always loaded at installation
+    'data': [],
+}
